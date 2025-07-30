2f1cd3c4905851b6bd413916687e8bb1a687dd90
DharaniMurugan01 <2k21dhara@gmail.com>
2025-07-30 07:22:24 +0530

    Add PIM feature, steps, CSV data, and CSV reader utility

--- src/test/features/PIM.feature
+++ src/test/features/PIM.feature
@@ -9,3 +9,12 @@ Scenario: Add a new employee
   | username | password  |
   | Admin    | admin123  |
   
+  Scenario: Add employee with Invalid Credentials
+  Given I want to go into OrangeHRM
+  When The user enters the username "<username>" and password "<password>" to login
+  And the user click the PIM menu
+  And the user fills the employee form with invalid credentials
+  Then the employee should not be added successfully
+  Examples:
+  | username | password  |
+  | Admin    | admin123  |
--- src/test/steps/PIMstep.ts
+++ src/test/steps/PIMstep.ts
@@ -1,8 +1,10 @@
 import { expect } from '@playwright/test';
-import { Given, Then, When } from "@cucumber/cucumber";
+import { Given, Then, When ,Before} from "@cucumber/cucumber";
 import { pageFixture } from "../../hooks/pageFixture";
 import employeedata from "../../helper/util/data/employeedata.json";
 import pimpage from "../pages/PIMPage";
+import { readCSV } from "../../helper/util/csvReader";
+import path from "path";
 let pim: pimpage;
 const fullName="";
          When('the user Add the employee',{ timeout: 20000 }, async function () {
@@ -20,3 +22,18 @@ const fullName="";
           //  await expect(nameLocator!).toHaveText(fullName);
           console.log("Employee added sucessfully");
          });
+         When('the user fills the employee form with invalid credentials',{timeout:30000},async function(){
+           pim = new pimpage(pageFixture.page!);
+           const filePath = path.resolve("src/helper/util/data/Invaliduser.csv"); 
+           const invalidData = await readCSV(filePath);
+           const emp = invalidData[0];
+           await pim.clickAdd();
+           await pim.FillForm(emp.Firstname, emp.Lastname, emp.Employeeid);
+           console.log(emp.Firstname, emp.Lastname, emp.Employeeid);
+           pageFixture.logger?.info(`Invalid Data -> Firstname: ${emp.Firstname}, Lastname: ${emp.Lastname}, EmployeeID: ${emp.Employeeid}`);
+            await pim.clicksave();
+           });
+         Then ('the employee should not be added successfully',async function(){
+          console.log("user get error message");
+         });
+        
